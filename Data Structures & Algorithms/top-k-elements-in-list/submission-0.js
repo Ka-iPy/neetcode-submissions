e298c21b52ab577b3469cadf49d566195cd845c7
Aireche Khalil <kaighost65@gmail.com>
2026-05-31 02:07:49 +0200

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let seen = {};
+
+        for(let num of nums){
+            if(!seen[num]){
+                seen[num] = 1
+            }
+            else {seen[num]++}
+        }
+        let hold = [];
+
+        for(let key of Object.keys(seen)){
+            hold.push({[key]:seen[key]})
+        }
+
+        let result = hold.sort((a, b) => {
+                    const aValue = Object.values(a)[0];
+                    const bValue = Object.values(b)[0];
+                    return bValue - aValue; // descending
+                }).map(a => a = Object.keys(a)[0]).filter((a,i) => i<k)
+        return result
+        
+    }
+}
