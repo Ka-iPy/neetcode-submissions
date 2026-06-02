ea17af25e82228c767164f854f2908a47a58bc52
Aireche Khalil <kaighost65@gmail.com>
2026-06-02 11:25:10 +0200

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let output = [];
+        for(let i = 0; i<nums.length; i++){
+            output.push(nums.filter((a,index) => index!== i).reduce((acc, curr) => { return acc*curr}, 1))
+            
+        }
+        return output
+    }
+}
